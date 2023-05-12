2928913e32e2815c1d15939e4c11e0f913ee7aaa
Joao HDB <jhdbarros@hotmail.com>
2023-05-12 16:56:09 -0300

    alteracao

--- src/main/kotlin/br/com/space/controleentrega/servidor/repository/PedidoCargaRepository.kt
+++ src/main/kotlin/br/com/space/controleentrega/servidor/repository/PedidoCargaRepository.kt
@@ -11,7 +11,7 @@ interface PedidoCargaRepository:CrudRepository<PedidoCarga,Int> {
     @Query("select ped_carga, ped_spvcodigo, ped_numero,ped_filcodigo, ped_stpcodigo,ped_valor, ped_pescodigo, pes_razao,\n" +
             " pes_fantasia, ped_qtdevol, ped_fpgcodigo, pes_cnpjcpf, fpg_desc, end_logradouro, end_numero,\n" +
             " end_complemento, end_fone1,end_celular,bai_desc, cid_desc, end_ufsigla, ped_ordement,coalesce(cen_codigo,0)as cen_codigo,coalesce(cen_ocecodigo,0)as cen_ocecodigo, coalesce(cen_parcial,0) as cen_parcial,\n" +
-            " coalesce((select max(not_snfnumero) from nota where not_lnpcodigo = ped_lnpcodigo and not_filcodigo = ped_filcodigo and ped_lnpcodigo <> 0),0) as numnota\n" +
+            " if(ped_lnpcodigo=0,ped_lnpcodigo-ped_lnpcodigo,(select max(not_snfnumero) from nota where not_lnpcodigo = ped_lnpcodigo)) as numnota\n" +
             " from pedidos left join contentregaped on ped_numero = cen_pednumero and ped_spvcodigo = cen_spvcodigo and\n" +
             " ped_filcodigo = cen_filcodigo left join ocorentrega on cen_ocecodigo = oce_codigo ,carga, pessoa, formapagto, enderecos, bairro, cidade\n" +
             " where ped_carga = car_codigo and ped_filcodigo = car_filcodigo and ped_pescodigo = pes_codigo and ped_fpgcodigo = fpg_codigo\n" +
@@ -22,7 +22,7 @@ interface PedidoCargaRepository:CrudRepository<PedidoCarga,Int> {
 
     @Query("select ped_filcodigo, ped_carga, ped_spvcodigo, ped_numero, ped_stpcodigo , ped_valor, ped_pescodigo, pes_razao, pes_fantasia, ped_qtdevol, ped_fpgcodigo,\n" +
             "pes_cnpjcpf, fpg_desc, end_logradouro, end_numero, end_complemento, bai_desc, cid_desc, end_ufsigla, end_fone1,end_celular, ped_ordement,cen_codigo,cen_ocecodigo,cen_parcial,\n" +
-            "coalesce((select max(not_snfnumero) from nota where not_lnpcodigo = ped_lnpcodigo and not_filcodigo = ped_filcodigo and ped_lnpcodigo <> 0),0) as numnota\n" +
+            "if(ped_lnpcodigo=0,ped_lnpcodigo-ped_lnpcodigo,(select max(not_snfnumero) from nota where not_lnpcodigo = ped_lnpcodigo)) as numnota\n" +
             "from pedidos left join contentregaped on ped_numero = cen_pednumero and ped_spvcodigo = cen_spvcodigo and ped_filcodigo = cen_filcodigo left join ocorentrega on cen_ocecodigo = oce_codigo ,carga, pessoa, formapagto, enderecos, bairro, cidade\n" +
             "where ped_carga = car_codigo and ped_filcodigo = car_filcodigo and ped_pescodigo = pes_codigo and ped_fpgcodigo = fpg_codigo\n" +
             "and ped_endentrega = end_codigo and ped_pescodigo = end_pescodigo and end_baicodigo = bai_codigo and end_cidcodigo = cid_codigo and cen_usrlogin = :usuario and (cen_ocecodigo = 0 or cen_parcial = 1) and cen_dataentrega = :dataEntrega " +
@@ -35,7 +35,7 @@ interface PedidoCargaRepository:CrudRepository<PedidoCarga,Int> {
     @Query("select ped_carga, ped_spvcodigo, ped_numero,ped_filcodigo, ped_stpcodigo,ped_valor, ped_pescodigo, pes_razao,\n" +
             "pes_fantasia, ped_qtdevol, ped_fpgcodigo, pes_cnpjcpf, fpg_desc, end_logradouro, end_numero,\n" +
             "end_complemento, end_fone1,end_celular,bai_desc, cid_desc, end_ufsigla, ped_ordement,coalesce(cen_codigo,0)as cen_codigo,coalesce(cen_ocecodigo,0)as cen_ocecodigo, coalesce(cen_parcial,0) as cen_parcial,\n" +
-            "coalesce((select max(not_snfnumero) from nota where not_lnpcodigo = ped_lnpcodigo and not_filcodigo = ped_filcodigo and ped_lnpcodigo <> 0),0) as numnota\n" +
+            "if(ped_lnpcodigo=0,ped_lnpcodigo-ped_lnpcodigo,(select max(not_snfnumero) from nota where not_lnpcodigo = ped_lnpcodigo)) as numnota\n" +
             "from pedidos left join contentregaped on ped_numero = cen_pednumero and ped_spvcodigo = cen_spvcodigo and\n" +
             "ped_filcodigo = cen_filcodigo left join ocorentrega on cen_ocecodigo = oce_codigo ,carga, pessoa, formapagto, enderecos, bairro, cidade\n" +
             "where ped_carga = car_codigo and ped_filcodigo = car_filcodigo and ped_pescodigo = pes_codigo and ped_fpgcodigo = fpg_codigo\n" +
